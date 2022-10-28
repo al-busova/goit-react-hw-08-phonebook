d01cf30578034c1417af63579701c801c1112ec7
Alena Busova <96522313+al-busova@users.noreply.github.com>
2022-10-28 19:57:25 +0300

    remove unnecessary

--- src/components/Contacts/ContactsList.jsx
+++ src/components/Contacts/ContactsList.jsx
@@ -2,14 +2,10 @@ import PropTypes from 'prop-types';
 import { ContactItem } from './ContactItem';
 import {ListContacts, ItemContacts} from './Contacts.styled'
 export const ContactList = ({ contacts }) => {
-  const contactsLoc = Object.values(contacts);
-  const filterContacts = contactsLoc.filter(contact => contact.id);
-  // console.log('contLoc', contactsLoc)
-  // console.log('filC', filterContacts)
-  // console.log('contacts in list',contacts)
+
   return (
     <ListContacts>
-      {filterContacts.map(contact => (
+      {contacts.map(contact => (
         <ItemContacts key={contact.id}>
           <ContactItem
             name={contact.name}
@@ -21,6 +17,7 @@ export const ContactList = ({ contacts }) => {
     </ListContacts>
   );
 };
+
 ContactList.propTypes = {
   conacts: PropTypes.arrayOf(
     PropTypes.shape({
