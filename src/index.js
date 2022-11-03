ebf7cdf6801b70c1c37a94b4eae780afc78e5c9a
Alena Busova <96522313+al-busova@users.noreply.github.com>
2022-11-03 19:00:09 +0200

    del persist

--- src/index.js
+++ src/index.js
@@ -4,18 +4,15 @@ import { App } from 'components/App';
 import { ThemeProvider } from '@emotion/react';
 import './index.css';
 import { theme } from './constants/theme';
-import { store, persistor } from './redux/store';
+import { store } from './redux/store';
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
         <ThemeProvider theme={theme}>
           <App />
         </ThemeProvider>
-      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
